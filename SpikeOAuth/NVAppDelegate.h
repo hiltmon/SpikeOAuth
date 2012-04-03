b7e6d8d4e00ac89e2ca77ac286113a6cdd5718bc
Hilton Lipschitz <hiltmon@gmail.com>
2012-04-03 13:53:25 -0400

    Initial Commit

---
+++ SpikeOAuth/NVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NVAppDelegate.h
+//  SpikeOAuth
+//
+//  Created by Hilton Lipschitz on 4/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NVAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
